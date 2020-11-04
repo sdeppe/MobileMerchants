ecbeb6237b0670a3fa19aafde5b84f4198526b4c
shawn <sdeppe@csumb.edu>
2020-11-04 22:45:45 +0900

    Added basic project outline

---
+++ app/src/main/java/com/example/mobilemerchants/FoodDisplay.java
@@ -0,0 +1,38 @@
+package com.example.mobilemerchants;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FoodDisplay extends AppCompatActivity {
+
+    TextView tvResturantName;
+    RecyclerView rvFoodDisplay;
+    Button btnGoBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_display);
+
+        tvResturantName = findViewById(R.id.tvResturantName);
+        rvFoodDisplay = findViewById(R.id.rvFoodDisplay);
+        btnGoBack = findViewById(R.id.btnGoBack);
+
+        // todo set restaurant name
+        // todo set RecycleViewer
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(FoodDisplay.this, RestaurantDisplay.class);
+                startActivity(i);
+            }
+        });
+    }
+}
